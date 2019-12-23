90d38e24c397b091c70d221f573cb15dd1bc4f7f
peipei28 <47714228+peipei28@users.noreply.github.com>
2019-12-23 14:45:11 +0800

    Add files via upload

---
+++ src/main/java/com/dominators/webScrapping.java
@@ -0,0 +1,34 @@
+package RTproject;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class webScrapping {
+
+    public static void main(String[] args) {
+
+        final String url = "https://github.com/STIW3054-A191/Assignments/issues/1";
+
+        try {
+            final Document document = Jsoup.connect(url).get();
+            Elements info = document.getElementsByClass("d-block comment-body markdown-body  js-comment-body");
+            Object object = new Object();
+            for (Element Info:info) {
+                String link = Info.getElementsByTag("a").attr("abs:href");
+                //System.out.println(link);
+                object.setUrlLink(link);
+            }
+
+            thread T1 = new thread(object);
+            T1.run();
+        }
+        catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+}
