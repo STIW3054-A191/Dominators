524ce8e651164a92d4d7a114807d5f73207802b7
X One <46247836+TanChengYi@users.noreply.github.com>
2020-01-01 17:07:59 +0800

    Add files via upload

---
+++ src/main/java/com/dominators/ClassMetricsContainer.java
@@ -0,0 +1,52 @@
+package com.dominators;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+
+public class ClassMetricsContainer {
+    /** The map from class names to the corresponding metrics */
+    private HashMap<String, ClassMetrics> m = new HashMap<String, ClassMetrics>();
+
+    /** Return a class's metrics */
+    public ClassMetrics getMetrics(String name) {
+        ClassMetrics cm = m.get(name);
+        if (cm == null) {
+            cm = new ClassMetrics();
+            m.put(name, cm);
+        }
+        return cm;
+    }
+
+    /** Print the metrics of all the visited classes. */
+    public void printMetrics(CkjmOutputHandler handler) {
+        Set<Map.Entry<String, ClassMetrics>> entries = m.entrySet();
+        Iterator<Map.Entry<String, ClassMetrics>> i;
+
+        for (i = entries.iterator(); i.hasNext(); ) {
+            Map.Entry<String, ClassMetrics> e = i.next();
+            ClassMetrics cm = e.getValue();
+            if (cm.isVisited() && (MetricsFilter.includeAll() || cm.isPublic()))
+                handler.handleClass(e.getKey(), cm);
+        }
+    }
+
+    public void calcTotal(){
+        Set<Map.Entry<String, ClassMetrics>> entries = m.entrySet();
+        Iterator<Map.Entry<String, ClassMetrics>> i;
+        int wmc=0;int dit=0;int noc=0; int cbo=0;int rfc=0;int lcom=0;
+
+        for (i = entries.iterator(); i.hasNext(); ) {
+            Map.Entry<String, ClassMetrics> e = i.next();
+            ClassMetrics cm = e.getValue();
+            if (cm.isVisited() && (MetricsFilter.includeAll() || cm.isPublic())){
+                wmc+=cm.getWmc();dit+=cm.getDit();noc+=cm.getNoc();cbo+=cm.getCbo();rfc+=cm.getRfc();lcom+=cm.getLcom();
+            }
+        }
+//        System.out.println(wmc+" "+dit+" "+noc+" "+cbo+" "+rfc+" "+lcom);
+    }
+
+
+}
